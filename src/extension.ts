41fc42ecc03281d0dd3ab2b6acc2353594cb82db
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-02-01 19:45:49 +0200

    implement the core features

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,116 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function compareSelection(a:vscode.Selection, b:vscode.Selection) {
+	if (a.start.line < b.start.line) {
+		return -1;
+	}
+	if (a.start.line > b.start.line) {
+		return 1;
+	}
+	if (a.start.character < b.start.character) {
+		return -1;
+	}
+	if (a.start.character > b.start.character) {
+		return 1;
+	}
+	return 0;
+}
+
+function indentOf(x:string) {
+	return x.substr(0, x.indexOf(x.trim()));
+}
+
+const input_handler = `(lambda: (__import__('json').dump(__builtins__.list(__builtins__.map(__builtins__.repr, __builtins__.map(eval, __import__('json').load(__import__('sys').stdin)))), __import__('sys').stdout)))()\n`;
+const default_context_py_content = `i = iter(range(1000000000))\n`;
+
+
 export function activate(context: vscode.ExtensionContext) {
+	let disposable1 = vscode.commands.registerCommand('extension.evaluate', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor === undefined) {
+			vscode.window.showErrorMessage('No active text editor');
+			return;
+		}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "evaluate-python-expression" is now active!');
+		let context_py = undefined;
+		if (vscode.workspace.rootPath !== undefined) {
+			context_py = path.resolve(vscode.workspace.rootPath, '.vscode', 'context.py');
+		}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		// get the content of the context.py
+		let context_py_content = default_context_py_content;
+		if (context_py !== undefined && fs.existsSync(context_py)) {
+			context_py_content = fs.readFileSync(context_py, 'utf8');
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// turn selection into JSON
+		let selections: vscode.Selection[] = editor.selections;
+		selections = selections.sort(compareSelection);
+
+		const input = JSON.stringify(selections.map(selection => editor.document.getText(selection).trim()));
+		let output = undefined;
+
+		// pass selection to Python
+		try {
+			const args = ['-c', context_py_content + '\n' + input_handler];
+			output = child_process.execFileSync('python', args, { encoding: 'utf8', input });
+		} catch (ex) {
+			console.log(ex);
+			vscode.window.showErrorMessage('' + ex);
+			return;
+		}
+
+		const results = JSON.parse(output);
+
+		// replace selection with result
+		editor.edit(builder => {
+			let index = 0;
+			for (const selection of selections) {
+				let text = editor.document.getText(selection);
+				builder.replace(selection, indentOf(text) + results[index]);
+				index += 1;
+			}
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	let disposable2 = vscode.commands.registerCommand('extension.create_context', () => {
+		// must have a workspace
+		if (vscode.workspace.rootPath === undefined) {
+			vscode.window.showErrorMessage('No workspace selected');
+			return;
+		}
+
+		let dot_vscode = path.resolve(vscode.workspace.rootPath, '.vscode');
+		let context_py = path.resolve(dot_vscode, 'context.py');
+
+		// context.py already exists
+		if (fs.existsSync(context_py)) {
+			vscode.workspace.openTextDocument(context_py).then(doc => {
+				vscode.window.showTextDocument(doc);
+			});
+			return;
+		}
+
+		// no .vscode folder
+		if (!fs.existsSync(dot_vscode)) {
+			fs.mkdirSync(dot_vscode);
+		}
+
+		// populate context.py
+		fs.writeFile(context_py, default_context_py_content, function (err) {
+			if (err) {
+				vscode.window.showErrorMessage('' + err);
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable1);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
